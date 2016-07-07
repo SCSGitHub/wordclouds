1fc569dd6eb1a15b1ca921954093fb5dcf17af7b
Sam Lapp <sammlapp@gmail.com>
2016-07-07 16:29:56 -0400

    demo and clouds working and look cool

--- static/wordclouds/scripts/cloud.js
+++ static/wordclouds/scripts/cloud.js
@@ -1,49 +1,63 @@
 //input data structures
-var input_sentence = getSentence(); 
-var input_senses={
-	"can":{
-		sense_number: 0,
-		sense_type: "n",
-		synonym_list: [ 
-			[
-				{word:"tent", sense:"tent.n.1", lemma:"tent"},
-				{word:"camp", sense:"camp.v.1", lemma:"camp"},
-			],
-			[
-				{word:"be", sense:"be.v.1", lemma:"be"},
-				{word:"stand", sense:"stand.v.1", lemma:"stand"},
-			],
-		],
-	},	
-	"entropy":{
-		sense_number: 1,
-		sense_type: "v",
-		synonym_list: [ 
-			[
-				{word:"chaos", sense:"chaos.n.1", lemma:"chaos"},
-				{word:"disorder", sense:"disorder.v.1", lemma:"disorder"},
-				{word:"diffusion", sense:"diffusion.v.1", lemma:"diffusion"},
-			],
-			[
-				{word:"wrong sense", sense:"wrong_sense.n.1", lemma:"wrond_sense"},
-				{word:"information", sense:"information.n.1", lemma:"information"},
-			],
-		],
-	},
-};
+var input_sentence = { sentence_id: 0, words:[]};
+var input_senses;//=[
+// 	{ word:"can",
+	//senses:[
+// 		{
+// 			sense_number: 0,
+// 			pos: "n",
+// 			synonym_list: 
+// 				[
+// 					{word:"tent", sense:"tent.n.1", lemma:"tent"},
+// 					{word:"camp", sense:"camp.v.1", lemma:"camp"},
+// 				],
+// 		},
+// 		{
+// 			sense_number: 1,
+// 			pos: "n",
+// 			synonym_list:
+// 				[
+// 					{word:"be", sense:"be.v.1", lemma:"be"},
+// 					{word:"stand", sense:"stand.v.1", lemma:"stand"},
+// 				],
+// 		},
+// 	]},	{ word:"entropy",
+// 		  senses:[ //array of senses
+// 		{
+// 			sense_number: 0,
+// 			pos: "n",
+// 			synonym_list: 
+// 				[
+// 					{word:"chaos", sense:"chaos.n.1", lemma:"chaos"},
+// 					{word:"disorder", sense:"disorder.n.1", lemma:"disorder"},
+// 					{word:"diffusion", sense:"diffusion.n.1", lemma:"diffusion"},
+// 				]
+// 		},
+// 		{
+// 			sense_number: 1,
+// 			pos: "n",
+// 			synonym_list:
+// 				[
+// 					{word:"wrong sense", sense:"wrong_sense.n.1", lemma:"wrond_sense"},
+// 					{word:"information", sense:"information.n.1", lemma:"information"},
+// 				],
+// 		},
+// 	]},
+// ];
 
-var sentence = input_sentence.words;
+var sentence = []; 
 //output data structure:
 var cloud = [ 
-	{
-		sentence_word:"sample word",
-		syn_list:
-			[ 
-				{word:"similar_word", sense:"similar_word.n.1", lemma:"lemma", abstraction_level: "concrete"},
-			]
-	},
+	// {
+	// 	sentence_word:"sample word",
+	// 	syn_list:
+	// 		[ 
+	// 			{word:"similar_word", sense:"similar_word.n.1", lemma:"lemma", abstraction_level: "concrete"},
+	// 		]
+	// },
 ];
 
+var problem_id = 1; //get as an input
 var score = 0; 
 
 //helper functions
@@ -71,29 +85,36 @@ function addWord(me){
 function loadSentence(){
 	var original_template = $("#word_sentence_template").html();
 
-
 	for (var i = 0; i<sentence.length; i++){
 		var word_of_sentence = sentence[i];
+
 		var li = $("#word_sentence_template").find("li");
 		$(li).attr("word",word_of_sentence);
 		var sentence_div = li.find(".word_column_div");
 		$(sentence_div).attr("id", "word_sentence_"+i+"");
 		$(sentence_div).find(".word_name").html(word_of_sentence);
 
-		//bins for synonym and abstract
-		var add_button = $("#add_button_template").html();
-		var concrete_list = sentence_div.find(".concrete");
-		var abstract_list = sentence_div.find(".abstract");
-		$(concrete_list).append(add_button);
-		$(abstract_list).append(add_button);
+		if(input_senses[i].senses.length==0){//no synonyms
+			$(sentence_div).addClass("no_synonyms");
+			$(sentence_div).find("div").hide();
+		}else{
+			//bins for synonym and abstract
+			var add_button = $("#add_button_template").html();
+			var concrete_list = sentence_div.find(".concrete");
+			var abstract_list = sentence_div.find(".abstract");
+			$(concrete_list).append(add_button);
+			$(abstract_list).append(add_button);
+		}
 		
 		var prepared_column = $("#word_sentence_template").html();
 		
 		$("#sentence").append(prepared_column);
 		$('div[id=word_sentence_'+i+']').collapsible({
+
 			expand: function(event){
 				var word = $(event.target).parent().attr("word");
-				loadSenses(word);
+				var wordNumber = sentence.findIndex(function(val){return (val==word)});
+				loadSenses(wordNumber);
 				//collapse the other columns
 				var allColumns = $('.word_column_div');
 				for(var j=0; j<allColumns.length; j++){
@@ -111,6 +132,8 @@ function loadSentence(){
 		$('.collapsible3').collapsible({
 			collapsed: false,
 		});
+		$('.no_synonyms').collapsible('disable');
+		$("#word_sentence_0").collapsible('expand');
 
 		$("#word_sentence_template").html(original_template);//reset
 		//also add it into the output structure
@@ -120,20 +143,20 @@ function loadSentence(){
 }
 
 //function to load word sense (suggestion list):
-function loadSenses(word_with_senses){
+function loadSenses(word_number){
 
   	var original_template = $("#word_sense_template").html();
 	var target = $("#left-menu").find(".ui-collapsible-content");
 	$(target[0]).html("");
 
 	try{
-		var senses = input_senses[word_with_senses].synonym_list;
+		var senses = input_senses[word_number].senses;
 	  	for(var j=0; j<senses.length; j++){
 			var sense = senses[j];
 			var sense_div = $("#word_sense_template").find("div");
 			var sense_list = sense_div.find("ul");
-			for (var i=0; i<sense.length; i++){
-				synonym = sense[i];
+			for (var i=0; i<sense.synonym_list.length; i++){
+				synonym = sense.synonym_list[i];
 				var new_word_li = $("#word_item_template").find("li");
 				var new_word_text = new_word_li.find(".word_text");
 				new_word_text.html(synonym.word);
@@ -165,7 +188,7 @@ function loadSenses(word_with_senses){
 	            	$("#score").html(score);
 	            }
 	        },
-	        beforeStop: function(ev, ui) {
+	        beforeStop: function(ev, ui) { 
 	            if ($(ui.item).hasClass('number') && $(ui.placeholder).parent()[0] != this) {
 	                $(this).sortable('cancel');
 	            }
@@ -174,12 +197,11 @@ function loadSenses(word_with_senses){
 	}catch(err){
 		//no senses defined
 	}
-
 }
 
 function submitCloud(){
 
-	if(score<20){
+	if(score<1){
 		alert("your score is only "+score+"! You can do better than that");
 		return;
 	}else if(confirm("Your score is "+score+". You should aim for a score of at least 40. Are you done with your cloud?")) {
@@ -191,6 +213,7 @@ function submitCloud(){
 	//what do we need to do? 
 	//for each word-object in cloud, fill in syn_list[] with each {word, sense, lemma}
 	var words_array = $("#sentence").find(".word_column");
+
 
 	for(var j = 0; j<words_array.length; j++){
 		var cloud_column = {sentence_word: "", syn_list:[]};
@@ -215,27 +238,87 @@ function submitCloud(){
 		cloud[j]=cloud_column;
 	}
 	console.log(cloud);
-	alert("Your response has been recorded");
+	var output = {problem_id: problem_id, cloud: cloud};
+	
+	$.post(url, output, function(){
+		alert("Your response has been recorded");
+		//give code for payment
+	})
+		.fail(function(){
+			alert("failed");
+	});
+
 }
 
-function getSentence(){
-	//get sentence for api
-	return {sentence_id: 0, user_id: 0, words: ["can", "entropy", "be", "reversed?"]};
-
+function getSensesFromInput(data){
+	input_senses = [];
+	var words = data.words;
+	for(var i=0; i<words.length; i++){
+		word = words[i];
+		input_senses[i]={word: word.surface_form, senses: []};
+		var senses = words[i].senses;
+		for(var j=0; j<senses.length; j++){
+			var sense = senses[j];
+			var new_sense =
+			{
+				sense_number: sense.sense_number,
+				pos: sense.pos,
+				synonym_list: [],
+			};
+			var synonyms=[];
+			var brothers = sense.brothers;
+			for(var k=0; k<brothers.length; k++){ //add brothers of word
+				new_sense.synonym_list.push( { word: brothers[k], sense: sense.sense, lemma: brothers[k] } );
+			}
+			var hypernyms = sense.hypernyms;
+			for(var l=0; l<hypernyms.length; l++){
+				var brothers = hypernyms[l].brothers;
+				for(var m=0; m<brothers.length; m++){ //add brothers of hypernyms
+					new_sense.synonym_list.push( { word: brothers[m], sense: hypernyms[l].sense, lemma: brothers[m] } );
+				}
+			}
+			input_senses[i].senses.push(new_sense);
+		}
+	}
+	return input_senses;//{sentence_id: 0, words: ["can", "entropy", "be", "reversed?"]};
 }
 
-function getSynonyms(){
-	var url = 'http://scsweb-d11.andrew.cmu.edu:81/wordclouds/synonyms/entropy.n.02';
+function getSentenceFromInput(data){
+	var input_sentence = { sentence_id: problem_id, words:[]};
+	var words = data.words;
+	for(var i=0; i<words.length; i++){
+		word = words[i];
+		input_sentence.words.push(word.surface_form);
+	}
+	sentence = input_sentence.words;
+	return input_sentence;
+}
 
-	$.get(url, function(data){
-		entropy_response = data;
+function getSynonyms(problem_no){
+	var url = 'http://scsweb-d11.andrew.cmu.edu:81/wordclouds/problem/'+problem_no;
+	// $.get(url, function(data){
+	// 	input_sentence = getSentenceFromInput(data);
+	// 	input_senses = getSensesFromInput(data);
+	// });
+	$.ajax({
+	     async: false,
+	     type: 'GET',
+	     url: url,
+	     success: function(data) {
+			input_sentence = getSentenceFromInput(data);
+			input_senses = getSensesFromInput(data);
+	     }
 	});
 }
 
-window.onload = $(function() {
+$(document).ready(function() {
 	cloud = [];
+	problem_id=1;//**
+	getSynonyms(problem_id);//get input data from api
+
+
+	loadSenses(0);
 	loadSentence();
-	loadSenses(input_sentence.words[0]);
 
 	$(".sort").sortable({
 		connectWith: '.word1',
@@ -251,6 +334,7 @@ window.onload = $(function() {
     	},
     	cancel: ".add_button",
 	}).disableSelection();
+
 });
 
 //event handlers
