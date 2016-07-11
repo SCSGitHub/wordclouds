55dc2e2098bdaee9a8c9be49eaa00e40d99c9a01
RB <rbungard@cs.cmu.edu>
2016-07-11 14:01:50 -0400

    Add a few more hardcoded problems

--- problems.py
+++ problems.py
@@ -27,7 +27,16 @@ class Problem():
         self.id = id
 
         #get desc. would pull form DB
-        self.desc = "selecting colors that evoke semantics to represent data"
+        if id == 1:
+            self.desc = "selecting colors that evoke semantics to represent data"
+        elif id == 2:
+            self.desc = "grayscaling color reduce saliency of object regions in image"
+        elif id == 3:
+            self.desc = "person perceives personalities that differ when reading typefaces"
+        elif id == 4:
+            self.desc = "properties of clothing affect how people perceive another person's height"
+        else:
+            self.desc = ""
 
         #split desc into words
         word_forms = self.desc.split(" ")
@@ -38,8 +47,8 @@ class Problem():
 
         #get the word senses
         #senses = {form: wordnet.synsets(lemma) for form, lemma in lemmas.items()}
-        senses = {'selecting': 'select.v.01', 'colors': 'color.n.01', 'that': '', 'resonate': 'evoke.v.01',
-            'semantics': 'semantics.n.02', 'to': '', 'represent':'represent.v.02', 'data': 'data.n.01'}
+        #senses = {'selecting': 'select.v.01', 'colors': 'color.n.01', 'that': '', 'resonate': 'evoke.v.01',
+        #    'semantics': 'semantics.n.02', 'to': '', 'represent':'represent.v.02', 'data': 'data.n.01'}
 
         #construct array of word objects based on senses
         self.__get_word_senses(word_forms)
