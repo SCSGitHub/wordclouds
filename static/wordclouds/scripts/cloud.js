f2ee21f043221ce19593d6dc7dfcffb843826ab7
Sam Lapp <sammlapp@gmail.com>
2016-07-11 15:22:06 -0400

    username to training forward not working

--- static/wordclouds/scripts/cloud.js
+++ static/wordclouds/scripts/cloud.js
@@ -17,6 +17,7 @@ var input_senses;
 // 	]},
 // ];
 var sentence = []; 
+var full_sentence = "";
 //output data structure:
 var cloud = [];
 	//[ {
@@ -58,6 +59,7 @@ function addWord(me){
 //function to load sentence
 function loadSentence(){
 	var original_template = $("#word_sentence_template").html();
+	$("#sentence_text").html("Sentence: "+full_sentence);
 
 	for (var i = 0; i<sentence.length; i++){
 		var word_of_sentence = sentence[i];
@@ -68,7 +70,8 @@ function loadSentence(){
 		$(sentence_div).attr("id", "word_sentence_"+i+"");
 		$(sentence_div).find(".word_name").html(word_of_sentence);
 
-		if(input_senses[i].senses.length==0){//no synonyms
+
+		if(isStopWord(word_of_sentence)){//stop word. gray-out, no synonyms
 			$(sentence_div).addClass("no_synonyms");
 			$(sentence_div).find("div").hide();
 		}else{
@@ -261,6 +264,7 @@ function getSentenceFromInput(data){
 		input_sentence.words.push(word.surface_form);
 	}
 	sentence = input_sentence.words; //store just the word-strings in an array for easy access
+	full_sentence = data.desc;
 	return input_sentence;
 }
 
@@ -319,6 +323,12 @@ $(document).ready(function() {
 	});
 
 });
+
+
+var stopwords = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"];
+function isStopWord(word) {
+    return stopwords.indexOf(word.toLowerCase()) > -1;
+}
 
 //event handlers
 $('body').on('click', '.ui-icon-delete', function(){
